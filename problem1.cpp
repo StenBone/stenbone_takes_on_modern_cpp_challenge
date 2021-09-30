b101515ba5bf2d1e6a07186ab1f3f78b3dcfbadb
Ben Stone <ben.stone@canal-ins.com>
2021-09-30 17:14:58 -0400

    completed problem 1, tested. needs varification from the book.

--- problem1.cpp
+++ problem1.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <cassert>
+
+/**
+ * @brief sum of all natural numbers divisible by 3 or 5, up to a limit
+ * 
+ * @param limit natural number which is our ceiling
+ * @return int 
+ */
+int sum_of_numbers_divisible_by_3_and_5(int limit) {
+    int num = 1, sum = 0;
+    while (num < limit)
+    {
+        if (num % 3 == 0 || num % 5 == 0) {
+            sum += num;
+        }
+        num++;
+    }
+    return sum;
+}
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    // 3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 = 78
+    assert(sum_of_numbers_divisible_by_3_and_5(20) == 78);
+    return 0;
 }
