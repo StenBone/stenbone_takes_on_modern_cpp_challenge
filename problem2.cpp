68e05d5eafed88315eead2904205d44420d5f6a6
Ben Stone <ben.stone@canal-ins.com>
2021-10-01 16:12:00 -0400

    Problem3 tested, Problem 4 started

--- problem2.cpp
+++ problem2.cpp
@@ -1,11 +1,28 @@
 /* Problem 2 */
 #include <cassert>
 
-int test() {
-    return 0;
+int greatest_common_divisor(int a, int b) {
+
+    // Euclid's Algorithm
+    while(a != b) {
+        if (a > b) {
+            a -= b; // a = a - b
+        }
+        else { // b > a
+            b -= a; // b = b - a
+        }
+    }    
+
+    return a;
 }
 
 int main(int argc, const char* argv[]) {
-    int desired_result = 0;
-    assert(test() == desired_result);
+    int desired_result = 4;
+    assert(greatest_common_divisor(12, 8) == desired_result);
+
+    desired_result = 1;
+    assert(greatest_common_divisor(7, 12) == desired_result);
+
+    desired_result = 1;
+    assert(greatest_common_divisor(11, 7) == desired_result);
 }
